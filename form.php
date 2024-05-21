a2df9396099f0de997de1efbb8193302c7f267e1
alielajami <161844424+alielajami@users.noreply.github.com>
2024-05-21 23:52:12 +0300

    Add files via upload

---
+++ form.php
@@ -0,0 +1,32 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "myDB";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $project = $_POST['project'];
+    $message = $_POST['message'];
+
+    $sql = "INSERT INTO MyGuests (name, email, project, message)
+    VALUES ('$name', '$email', '$project', '$message')";
+
+    if ($conn->query($sql) === TRUE) {
+      echo "New record created successfully";
+    } else {
+      echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+
+$conn->close();
+?>
